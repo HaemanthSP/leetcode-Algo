6f7b0bed44efa472d96ded684b32c1c7611483e3
Haemanth S P <haemanthsp@gmail.com>
2020-03-18 12:30:07 +0100

    Solve 'Longest Palindromic Substring'

---
+++ 5_longest_palindromic_substring/README.md
@@ -0,0 +1,11 @@
+# [Longest Palindromic Substring](https://leetcode.com/problems/longest-palindromic-substring/)
+
+Example:
+--------
+
+> Input: "babad"  
+> Output: "bab"  
+> Note: "aba" is also a valid answer.  
+
+> Input: "cbbd"  
+> Output: "bb"
---
+++ 5_longest_palindromic_substring/longest_palindromic_substrig.py
@@ -0,0 +1,40 @@
+class Solution:
+    def get_palindrome(self, idx, s, centered=False):
+        palindrome = ''
+        
+        # Set the seed indices
+        x = idx-1
+        y = idx
+        if centered:
+            y = idx+1
+            palindrome = s[idx]
+           
+        for i in range(500):  # 500: as the max lenght is 1000
+            if s[x-i] == s[y+i] and (x-i >= 0) and (y+i < len(s)):
+                palindrome = s[x-i] + palindrome + s[y+i]
+            else:
+                break
+                
+        return palindrome 
+    
+    def longestPalindrome(self, s: str) -> str:
+        
+        palindrome = s[0] if s else ''
+        buffers = []
+        for idx in range(1, len(s)-1):
+            # Detect the two type of centers of palindrome
+            if s[idx-1] == s[idx]:
+                buffers.append(self.get_palindrome(idx, s))
+            elif s[idx-1] == s[idx+1]:
+                buffers.append(self.get_palindrome(idx, s, centered=True))
+                
+        # Handle the border case
+        if s[-1] == s[-2]:
+            buffers.append(s[-2] + s[-1])
+            
+        # Find the longest
+        for buffer in buffers:
+            if len(buffer) > len(palindrome):
+                palindrome = buffer
+                
+        return palindrome
